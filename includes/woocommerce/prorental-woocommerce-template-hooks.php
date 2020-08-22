2f8ef089a5c3577d8903d1d96f809c0227ea891d
cjwd <chinarakinda@gmail.com>
2020-08-22 13:38:20 -0400

    Remove related products from single page

--- includes/woocommerce/prorental-woocommerce-template-hooks.php
+++ includes/woocommerce/prorental-woocommerce-template-hooks.php
@@ -6,6 +6,7 @@
 remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
+remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 20);
 remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
 remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
