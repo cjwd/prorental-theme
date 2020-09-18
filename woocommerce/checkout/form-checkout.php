a119d55ad4f3b99e189516e3762f485f1f0f0520
cjwd <chinarakinda@gmail.com>
2020-09-18 11:39:32 -0400

    Custom template for checkout page

---
+++ woocommerce/checkout/form-checkout.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Checkout Form
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-checkout.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.5.0
+ */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+do_action('woocommerce_before_checkout_form', $checkout);
+
+// If checkout registration is disabled and not logged in, the user cannot checkout.
+if (!$checkout->is_registration_enabled() && $checkout->is_registration_required() && !is_user_logged_in()) {
+  echo esc_html(apply_filters('woocommerce_checkout_must_be_logged_in_message', __('You must be logged in to checkout.', 'woocommerce')));
+  return;
+}
+
+?>
+
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
+  <div class="u-flex">
+    <?php if ($checkout->get_checkout_fields()) : ?>
+      <div id="customer_details" class="u-w-2/3">
+        <?php do_action('woocommerce_checkout_before_customer_details'); ?>
+        <fieldset class="o-step">
+          <legend class="u-hidden-visually">Billing Details</legend>
+          <div class="o-step__heading">
+            <div class="o-step__title">Billing Details</div>
+          </div>
+          <div class="o-step__container">
+            <p class="o-step__description">Description here....</p>
+            <div class="o-step__content">
+              <?php do_action('woocommerce_checkout_billing'); ?>
+            </div>
+          </div>
+        </fieldset>
+
+        <?php
+        /**
+         * @hooked secondary_contact_checkout_fields - 10
+         */
+        do_action('prorental_after_checkout_billing_form');
+        ?>
+
+        <fieldset class="o-step">
+          <legend class="u-hidden-visually">Shipping Details</legend>
+          <div class="o-step__heading">
+            <div class="o-step__title">Shipping Details</div>
+          </div>
+          <div class="o-step__container">
+            <p class="o-step__description">Description here....</p>
+            <div class="o-step__content">
+              <?php do_action('woocommerce_checkout_shipping'); ?>
+            </div>
+          </div>
+        </fieldset>
+        <fieldset class="o-step">
+          <legend class="u-hidden-visually">Payment</legend>
+          <div class="o-step__heading">
+            <div class="o-step__title">Payment</div>
+          </div>
+          <div class="o-step__container">
+            <p class="o-step__description">Description here....</p>
+            <div class="o-step__content">
+              <?php
+              /**
+               * @hooked woocommerce_checkout_payment - 10
+               */
+              do_action('woocommerce_checkout_after_customer_details');
+              ?>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+    <?php endif; ?>
+
+    <?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
+
+    <?php do_action('woocommerce_checkout_before_order_review'); ?>
+
+    <div id="order_review" class="u-w-1/3 woocommerce-checkout-review-order">
+      <h3 id="order_review_heading"><?php esc_html_e('Order Summary', 'woocommerce'); ?></h3>
+      <?php
+      /**
+       * @hooked woocommerce_order_review - 10
+       * @unhooked woocommerce_checkout_payment - 20
+       */
+      do_action('woocommerce_checkout_order_review');
+      ?>
+    </div>
+
+    <?php do_action('woocommerce_checkout_after_order_review'); ?>
+  </div>
+</form>
+
+<?php do_action('woocommerce_after_checkout_form', $checkout); ?>
