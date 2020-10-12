3d28edc87737695bf1c06da4f0626b66bc898ab2
cjwd <chinarakinda@gmail.com>
2020-10-12 15:05:41 -0400

    Fix: Pre-filled and newly-filled field values are being obscured by field label

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,12 +4,24 @@ const formCheckoutEl = document.getElementById('woocommerce-checkout'),
 
 if (formCheckoutEl.length > 0) {
   checkoutInputs.forEach((e) => {
+    if (e.value.length > 0) {
+      e.parentElement.parentElement.classList.add('is-filled');
+    }
+
     e.addEventListener('focus', (el) => {
       el.target.parentElement.parentElement.classList.add('is-active');
     });
 
     e.addEventListener('blur', (el) => {
-      el.target.parentElement.parentElement.classList.remove('is-active');
+      let targetEl = el.target,
+        wrapperEl = targetEl.parentElement.parentElement;
+
+      targetEl.parentElement.parentElement.classList.remove('is-active');
+      if (targetEl.value.length > 0) {
+        wrapperEl.classList.add('is-filled');
+      } else {
+        wrapperEl.classList.remove('is-filled');
+      }
     });
   });
 }
