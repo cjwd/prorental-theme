eaf2e3027c06257d02585ee7a7c7b3c268c06b83
cjwd <chinarakinda@gmail.com>
2020-12-18 15:34:21 -0400

    Remove placeholder widget from footer

--- footer.php
+++ footer.php
@@ -1,17 +1,6 @@
   <footer class="off-white-bg">
     <div class="footer-top pt-50 pb-60">
       <div class="container">
-        <div class="single-footer">
-          <h3>Contact us</h3>
-          <div class="footer-content">
-            <div class="loc-address">
-              <span><i class="fa fa-map-marker"></i>184 Main Rd E, St Albans VIC 3021, Australia.</span>
-              <span><i class="fa fa-envelope-o"></i>Mail Us : yourinfo@example.com</span>
-              <span><i class="fa fa-phone"></i>Phone: + 00 254 254565 54</span>
-            </div>
-            <div class="payment-mth"><a href="#"><img class="img" src="<?php echo get_theme_file_uri('dist/assets/img/footer/1.png'); ?>" alt="payment-image"></a></div>
-          </div>
-        </div>
         <?php Slate\widgetize_footer('sidebar-footer'); ?>
       </div>
     </div>
